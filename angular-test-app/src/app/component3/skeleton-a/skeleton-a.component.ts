560b72794a9dc212af5d28536f90a47795ef6f80
Nazar-git <61249694+Nazar-git@users.noreply.github.com>
2020-12-27 22:44:07 +0200

    1.3

--- angular-test-app/src/app/component3/skeleton-a/skeleton-a.component.ts
+++ angular-test-app/src/app/component3/skeleton-a/skeleton-a.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NEWSLIST} from "../../shared/NEWSMOCKS";
+import {NEWS} from "../../shared/NEWS_INTERFACE";
 @Component({
   selector: 'app-skeleton-a',
   templateUrl: './skeleton-a.component.html',
   styleUrls: ['./skeleton-a.component.css']
 })
 export class SkeletonAComponent implements OnInit {
+tipeher: NEWS 
+tipe:  string[]
 
   constructor() { }
 
   ngOnInit() {
+    this.tipeher = NEWSLIST[0] 
+    this.tipe = this.tipeher.main
   }
 
 }
