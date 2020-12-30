083dc91722e852946b5fefee347821bc8076120e
Bogdan Tokar <32542502+BogdanIsLaugh@users.noreply.github.com>
2020-12-30 13:20:06 +0200

    0.3 from Bogdan

--- angular-test-app/src/app/app-routing.module.ts
+++ angular-test-app/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MainPageComponent } from "./component1/main-page/main-page.component";
-import { SkeletonComponent } from "./component2/skeleton/skeleton.component";
+import { AddPostComponent } from "./component2/Addpost/add-post.component";
 import { SkeletonAComponent } from "./component3/skeleton-a/skeleton-a.component";
 //import { SkeletonBComponent } from "./component4/skeleton-b/skeleton-b.component"
 
 const routes: Routes = [
   { path: "", component: MainPageComponent },
   { path: "main1", component: MainPageComponent },
-  { path: "main2", component: SkeletonComponent },
+  { path: "main2", component: AddPostComponent },
   { path: "main3", component: SkeletonAComponent }
   //{path:"main4",component: SkeletonBComponent },
 ];
